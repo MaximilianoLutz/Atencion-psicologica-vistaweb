4e824e87fd81845fea70b1b02c38d05448d5c42f
unknown <maximilianolutz@hotmail.com>
2023-03-20 20:58:28 +0100

    comienzo forgotten password

---
+++ src/screens/auth/ForgottenPassword.js
@@ -0,0 +1,71 @@
+import { Box, Button, CssBaseline, FormControlLabel, Grid, TextField, Typography } from '@mui/material';
+import React from 'react';
+
+
+export const ForgottenPassword = () => {
+
+  
+  const [ formValues, handleInputChange ] = useForm({email: ''});
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // const url = `http://${ ip }:8080/api/user`;
+
+    // const crearUser = await crearPaciente(url, formValues);
+    
+    // if (crearUser[1] === 201){
+    //   dispatch(startLogin({email, password}));
+    //   navigate('/');
+
+    // }
+  };
+
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'success.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Restaurar contraseña
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              value={email}
+              autoComplete="email"
+              autoFocus
+              onChange={handleInputChange}
+            />
+           
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, bgcolor: 'warning.main' }}
+            >
+              Restaurar
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  )
+}
